457f6b346cde8c3e75768f41d34376758e9cb7c9
nebab <bane.bmb@gmail.com>
2018-01-17 20:52:07 +0100

    ajmodalje

--- app/src/main/java/com/example/code/tiktak/MainActivity.java
+++ app/src/main/java/com/example/code/tiktak/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.code.tiktak;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int button [] = {R.id.a1 , R.id.a2 ,R.id.a3 ,R.id.b1 ,R.id.b2 ,
+                            R.id.b3 ,R.id.c1 ,R.id.c2,R.id.c3 };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int i=0; i<button.length; i++)
+
+
+
+
+
+
     }
 }
